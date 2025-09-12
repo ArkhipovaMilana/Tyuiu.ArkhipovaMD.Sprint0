1a4f8d344a8f96a186c868faf555d958d48c6934
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-09-12 20:11:45 +0500

    Task1

---
+++ Tyuiy.ArkhipovaMD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiy.ArkhipovaMD.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Милана Хаметова";
+            int age = 13;
+            Console.WriteLine("Привет, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
